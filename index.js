898db0de0c6674edb7cd0292f44ef44c433582f2
ThunderfuryofWindseeker <nedyalkov1717@gmail.com>
2023-03-17 00:31:21 +0200

    .

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ console.log('bb');
 // =========================Promise. Что это, как работает=======================//
 //===========================================LESSON-5=================================================//
 
-console.log('Request data..');
+// console.log('Request data..');
 
 // setTimeout(() => {
 //     console.log('Preparing data .. ');
@@ -150,61 +150,61 @@ console.log('Request data..');
 //     },3000)
 // }, 2000);
 
-const p = new Promise(function (resolve, reject) {
-    setTimeout(() => {
-        console.log('Preparing data ...');
-        const backendData = {
-            server: 'aws',
-            port: 2000,
-            data: 'working'
-        };
-        resolve(backendData)
-    }, 2000)
-});
-p.then(data => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            data.info = true;
-            resolve(data)
-        }, 3000)
-    });
-}) 
-.then(clientData => { 
-    console.log('Data recived', clientData);
-    return clientData;
-}).then(data => {
-    data.promisInfo = 'NEO';
-    console.log ('Modified', data)
-}) // таким образом, посредством .then можно формировать цепочку(чейн).
-.catch( err => console.error('Error: ',err))     // усли вместо resolve - reject, то методом .catch мы ловим и выводим ошибку.
-.finally(() => console.log('finally'))
+// const p = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         console.log('Preparing data ...');
+//         const backendData = {
+//             server: 'aws',
+//             port: 2000,
+//             data: 'working'
+//         };
+//         resolve(backendData)
+//     }, 2000)
+// });
+// p.then(data => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             data.info = true;
+//             resolve(data)
+//         }, 3000)
+//     });
+// })
+//     .then(clientData => {
+//         console.log('Data recived', clientData);
+//         return clientData;
+//     }).then(data => {
+//         data.promisInfo = 'NEO';
+//         console.log('Modified', data)
+//     }) // таким образом, посредством .then можно формировать цепочку(чейн).
+// .catch(err => console.error('Error: ', err))     // усли вместо resolve - reject, то методом .catch мы ловим и выводим ошибку.
+// .finally(() => console.log('finally'))
 
-const sleep = ms => {
-    return new Promise(resolve => {
-        setTimeout(()=> resolve(),ms)
-    })
-};
+// const sleep = ms => {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve(), ms)
+//     })
+// };
 // sleep(20000).then(()=> console.log('After 2 sec'));
 
-Promise.all([sleep(10000), sleep(15000)]).then(()=> {
-    console.log('All finally')
-}); //метод All выполняет условия только когда все промисы закончатся.
+// Promise.all([sleep(10000), sleep(15000)]).then(() => {
+//     console.log('All finally')
+// }); //метод All выполняет условия только когда все промисы закончатся.
 
-Promise.race([sleep(10000), sleep(15000)]).then(()=> {
-    console.log('Race promises')
-}) // отработает самый первый.
+// Promise.race([sleep(10000), sleep(15000)]).then(() => {
+//     console.log('Race promises')
+// }) // отработает самый первый.
 
 // =========================Объекты с Object.create. Что такое getters, setters=======================//
 //===========================================LESSON-6=================================================//
-const pers = Object.create({},{
+const pers = Object.create({}, {
     name: {
         value: 'Anton',
         enumerable: true,
         writable: true,
-        configurable : true
+        configurable: true
     },
-    birthYear:{
-        value:1998,
+    birthYear: {
+        value: 1998,
         enumerable: true,
         writable: true,
         configurable: false
@@ -213,7 +213,7 @@ const pers = Object.create({},{
         get() {
             return new Date().getFullYear() - this.birthYear
         },
-        set(value){
+        set(value) {
             document.body.style.background = 'red';
             console.log('Set age', value)
         }
@@ -221,5 +221,185 @@ const pers = Object.create({},{
 });
 
 for (let keyq in pers) {
-    console.log( 'Key' ,keyq, pers[keyq])
+    console.log('Key', keyq, pers[keyq])
+};
+// =========================Все о ES6 Классах=======================//
+//===========================================LESSON-7=================================================//
+
+class Animal {
+    static type = 'ANIMAL'
+    constructor(options) {
+        this.name = options.name
+        this.age = options.age
+        this.hasTail = options.hasTail
+    }
+    voice() {
+        console.log('Im animal')
+    }
+};
+
+const animal = new Animal({
+    name: 'Animal',
+    age: 5,
+    hasTail: true
+})
+
+class Cat extends Animal {
+    constructor(options) {
+        super(options)
+        this.color = options.color
+    }
+    voice() {
+        console.log('Im pus in bus')
+    }
+    get ageInfo() {
+        return this.age * 7;
+    }
+    set ageInfo(newAge) {
+        this.age = newAge;
+    }
 }
+const cat = new Cat({
+    name: 'cat',
+    age: 2,
+    hasTail: true,
+    color: 'black'
+});
+
+//===========================================LESSON-7=================================================//
+//===========================================Практика=================================================//
+
+class Component {
+    constructor(selector) {
+        this.$el = document.querySelector(selector);
+    }
+    hide() {
+        this.$el.style.display = 'none'
+    }
+    show() {
+        this.$el.style.display = 'block'
+    }
+};
+class Box extends Component {
+    constructor(options) {
+        super(options.selector);
+        this.$el.style.width = this.$el.style.height = options.size + 'px';
+        this.$el.style.background = options.color;
+    }
+};
+const box1 = new Box({
+    selector: '#box1',
+    size: 100,
+    color: 'red'
+});
+const box2 = new Box({
+    selector: '#box2',
+    size: 100,
+    color: 'blue'
+});
+
+class Circle extends Box {
+    constructor(options) {
+        super(options)
+        this.$el.style.borderRadius = '50%'
+    }
+};
+const c = new Circle({
+    selector: '#circle',
+    size: 90,
+    color: 'green'
+});
+
+// =========================Как работает Async, Await. Работа с сервером c fetch=======================//
+//===========================================LESSON-8=================================================//
+
+// const delay = (ms) => {
+//     return new Promise(r => setTimeout(() => r(), ms))
+// };
+
+// const url = 'https://jsonplaceholder.typicode.com/todos';
+
+// function fetchTodos() {
+//     console.log('Fetch todo started..')
+//     return delay(2000)
+//     .then(()=>{
+//         return fetch(url)
+//     })
+//     .then(response => response.json())
+// };
+// fetchTodos()
+// .then(data => {
+//     console.log('Data:', data)
+// })
+// .catch(e => console.error(e))
+
+// async function fetchAsyncTodos(){
+//     try {
+//         console.log('Fetch todo started..')
+//         await delay(2000)
+//         const response = await fetch(url)
+//         const data = await response.json()
+//         console.log('Data:', data)
+//     } 
+//     catch(e){
+//         console.error(e)
+//     }
+
+// }
+// fetchAsyncTodos()
+
+// ========================= Proxy. Объекты, функции, классы. Часть 1=======================//
+//===========================================LESSON-9=================================================//
+//objects
+const persi = {
+    name: 'Anton',
+    age: 24,
+    job: 'Fullstack'
+};
+
+const op = new Proxy(persi, {
+    get(target, prop) {
+        console.log(`Getting prop ${prop}`)
+        return target[prop]
+    },
+    set(target, prop, value) {
+        if (prop in target) {
+            target[prop] = value
+        }
+        else {
+            throw new Error(`No ${prop} field in target`)
+        }
+    },
+    has(target, prop) {
+        return ['name', 'age', 'job'].includes(prop)
+    },
+    deleteProperty(target, prop) {
+        console.log('deletitng ..', prop)
+        delete target[prop]
+        return true
+    }
+});
+//functions
+const log = text => `Log: ${text}`;
+
+const fp = new Proxy(log, {
+    apply(target, thisArg, args) {
+        console.log('Calling fn')
+        return target.apply(thisArg, args)
+    }
+});
+//Classes
+class Persic {
+    constructor(name,age) {
+        this.name = name
+        this.age = age 
+    }
+};
+const PersonProxy = new Proxy(Persic, {
+    construct(target, args) {
+        console.log('Construct..')
+        return new target(...args)
+    }
+});
+
+const p = new PersonProxy('Anton', 24);
